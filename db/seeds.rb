c8e7cea0e777659ee3abf66bf71c20ae532e4912
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-16 11:04:47 +0900

    add seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+table_names = %w(
+  users
+)
+
+table_names.each do |table_name|
+  # 環境によってパスを変更する
+  path = Rails.root.join("db/seeds/#{Rails.env}/#{table_name}.rb")
+
+  # パスにファイルが存在しない場合はdevelopmentディレクトリを読み込む
+  path = path.sub(Rails.env, "development") unless File.exist?(path)
+
+  # 確認用に現在の読み込みファイルを表示
+  puts "#{table_name}..."
+
+  # ファイルを読み込む
+  require path
+end
