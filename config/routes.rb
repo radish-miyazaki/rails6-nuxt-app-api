2fe1cd7d79c6e92870b14b4806e8edf32b50fc80
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-19 15:16:16 +0900

    add projects controller file

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
       resources :user_token, only:[:create] do
         delete :destroy, on: :collection
       end
+
+      # projects
+      resources :projects, only: [:index]
     end
   end
 
